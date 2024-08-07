29be83b4292038fb859a3b5a2ee3b4dcb3377b3b
Alex <a.serb2014@yandex.ru>
2024-08-07 18:40:53 +0300

    07.08.24

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,9 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+
+import datetime
 
 
 def home_view(request):
@@ -8,8 +12,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,13 +27,17 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    try:
+        files = os.listdir(path=".")
+        return HttpResponse([i for i in str(files)])
+        # по аналогии с `time_view`, напишите код,
+        # который возвращает список файлов в рабочей
+        # директории
+    except NotImplemented:
+        raise NotImplemented('Список пуст')
